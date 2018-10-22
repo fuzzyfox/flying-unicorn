1f574a8aa16a4cef2fc471d442aa56dd510151a4
William Duyck <fuzzyfox0@gmail.com>
2018-10-22 14:55:34 +0100

    fix shallow shifts

--- app/Http/Resources/ShiftShallow.php
+++ app/Http/Resources/ShiftShallow.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Shift extends JsonResource
+class ShiftShallow extends JsonResource
 {
     /**
      * Transform the resource into an array.
