ee4ae0989a1d9b9fe7eb3f563b4cb50323ac6d4b
William Duyck <fuzzyfox0@gmail.com>
2018-10-23 01:15:19 +0100

    improve link display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,24 @@
         <b-row class="mb-3">
             <b-col cols="12">
                 <b-card title="Schedule">
-                    <fu-calednar :events="events"></fu-calednar>
-                    <code>{{ url('') }}/calendar/{{ Auth::user()->id }}.ics</code>
+                    <fu-calednar :events="events" class="mb-3"></fu-calednar>
+
+
+                    <b-card
+                        border-variant="danger"
+                        header="Secret Calendar Link:"
+                        header-border-variant="danger"
+                        header-text-variant="danger"
+                    >
+                        <p>
+                            Below you will find you personal calendar feed for
+                            your calendar application of choice to subscribe to.
+                            This is unique to you're account so
+                            <strong>do not</strong> share this link.
+                        </p>
+                        <pre><code>{{ preg_replace('/^https?:\/\//i', 'https://', url('')) }}/calendar/{{ Auth::user()->id }}.ics</code></pre>
+                        <b-btn href="{{ preg_replace('/^https?:\/\//i', 'webcal://', url('')) }}/calendar/{{ Auth::user()->id }}.ics">Add To Calendar</b-btn>
+                    </b-card>
                 </b-card>
             </b-col>
         </b-row>
