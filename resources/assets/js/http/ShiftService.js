6a0bfc138d22dd7dc1fd86eae53bfce302018cdb
William Duyck <fuzzyfox0@gmail.com>
2018-10-27 00:57:06 +0100

    dsfg

--- resources/assets/js/http/ShiftService.js
+++ resources/assets/js/http/ShiftService.js
@@ -7,4 +7,6 @@ export const update = (shiftId, userObj) => axios.put(`${root}/${shiftId}`, user
 export const destroy = (shiftId) => axios.delete(`${root}/${shiftId}`, userObj)
 
 export const storeUser = (shiftId, userId) => axios.post(`${root}/${shiftId}/users`, {user_id: userId})
+export const checkinUser = (shiftId, userId) => axios.post(`${root}/${shiftId}/checkin`, {user_id: userId})
+export const verifyUser = (shiftId, userId) => axios.post(`${root}/${shiftId}/verify`, {user_id: userId})
 export const destroyUser = (shiftId, userId) => axios.delete(`${root}/${shiftId}/users/${userId}`)
